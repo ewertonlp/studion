0986a4a59a7c2d099146cea951ee28a9e8eedc2d
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2021-07-10 19:16:57 -0300

    feat(Final): Testimonial and Gallery pages.

--- src/script.js
+++ src/script.js
@@ -1,11 +1,32 @@
 $(document).ready(() => {
+ // MENU
+ $('.navbar__menu-btn').on('click', function () {
+    $('.navbar__links').toggleClass('active');
+    $(this).find('i').toggleClass('fa-bars');
+    $(this).find('i').toggleClass('fa-times');
+ });
+
+
+
+ // CAROUSSEL
     const slickOptions = {
         autoplay: true,  
         dots: false,  
-        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
-        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Next</button>;',
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow slick-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow slick-arrow">Next</button>;',
     };
     $('.slider').slick(slickOptions);
+
+
+    // SLIDER TESTIMONIAL
+    const slickOptionsTestimonial = {
+      autoplay: true,  
+      dots: true,  
+      prevArrow: '<button type="button" class="slick-prev slider__prev-arrow slick-arrow">Previous</button>;',
+      nextArrow: '<button type="button" class="slick-next slider__next-arrow slick-arrow">Next</button>;',
+  };
+  $('.slider-testimonial').slick(slickOptionsTestimonial);
+
 
     $('.footer__form-button').on('click', () => {
         const email = $('#email').val();
